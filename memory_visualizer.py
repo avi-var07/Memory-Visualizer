f81f52fbaf2bf1af29a0852e85f17e79ec14cf5b
Aviral Varshney <aviralvarshney07@gmail.com>
2025-04-09 12:38:15 +0530

    Initialised the memory_visualizer

---
+++ memory_visualizer.py
@@ -0,0 +1,11 @@
+# memory_visualizer.py
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# Placeholder for main app
+root = tk.Tk()
+root.title("Memory Management Visualizer")
+root.geometry("800x600")
+root.mainloop()
